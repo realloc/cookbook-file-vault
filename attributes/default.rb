c3ea3ad147ada8f68c5bf8d44934b3239901740f
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2015-03-05 17:05:55 +0300

    Update README

---
+++ attributes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: file-vault
+# Attributes:: default
+#
+
+# Default data-bag to use for files
+default['file-vault']['bag'] = 'file-vault'
+
+# List of files to be deployed
+default['file-vault']['files'] = []
--- metadata.rb
+++ metadata.rb
@@ -6,3 +6,8 @@ description      'Get secret files from chef-vault!'
 long_description 'Simple cookbook to get secret files defined in attrivbutes from chef-vault'
 version          '0.0.1'
 
+%w{ubuntu debian redhat centos fedora}.each do |os|
+  supports os
+end
+
+depends 'chef-vault', '~> 1.0.4'
