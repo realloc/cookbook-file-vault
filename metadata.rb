c30247fca4907f325d3423c282ab55e54b2cd9bf
Stanislav Bogatyrev <realloc@realloc.spb.ru>
2015-03-06 15:30:54 +0300

    Bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,9 @@ maintainer_email 'realloc@realloc.spb.ru'
 license 'Apache 2.0'
 description 'Get secret files from chef-vault!'
 long_description 'Get secret files from chef-vault!'
-version '0.0.1'
+version '0.0.2'
+source_url 'https://github.com/realloc/cookbook-file-vault'
+issues_url 'https://github.com/realloc/cookbook-file-vault/issues'
 
 %w(ubuntu debian redhat centos fedora).each do |os|
   supports os
